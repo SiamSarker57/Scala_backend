ce847041018cb4f8195f25fe9850e733f4e16988
Md. Siam Sarker <siamsarker65@gmail.com>
2026-04-03 21:24:15 +0600

    Update settings.py

--- scala_guard/settings.py
+++ scala_guard/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-scala-guard-secret-key-2024'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
